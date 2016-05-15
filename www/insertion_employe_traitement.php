f350e8065ffe5c2f150a7853926a95b642cbbff0
Anton <aippolit@etu.utc.fr>
2016-05-15 12:22:12 +0200

    Insertion employe

---
+++ www/insertion_employe_traitement.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+        <title>Nouveau employé</title>
+    </head>
+    <body>        
+        <?php
+            include "connect.php";
+            $conn = fConnect();
+
+            //récuperation des données du formulaire
+            $num_badge = $_POST['num_badge'];
+            $nom = $_POST['nom'];
+            $prenom = $_POST['prenom'];
+            $email = $_POST['email'];
+            $statut = $_POST['statut'];
+            $nom_salle = $_POST['nom_salle'];
+
+            $sql = "INSERT INTO employe VALUES ($num_badge, '$nom', '$prenom', '$email', '$statut', '$nom_salle')";
+            $query = pg_query($conn, $sql);
+            if (!$query) {
+                echo "<h1> Erreur lors de l'insertion! </h1>";
+            }
+            else {
+                echo "<h1> Insertion réussie! </h1>";
+            }
+            pg_close($conn);
+        ?>
+        </form>
+        <a href="index.html"> Retour à l'accueil </a>
+    </body>
+</html>
